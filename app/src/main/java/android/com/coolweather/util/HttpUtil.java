f0d7e7ff4413d09697cf79ef16b5438f5dec6ad2
wanghao <1208922591@qq.com>
2017-08-20 10:14:53 +0800

    列出城市列表

---
+++ app/src/main/java/android/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package android.com.coolweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by JYDX on 2017/8/20.
+ */
+
+public class HttpUtil {
+        public static void sendOkHttpRequest(String address, Callback callback){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(address).build();
+            client.newCall(request).enqueue(callback);
+        }
+}
